93e76f968f9b05f66a212419c0afa64fcd09a997
seba0413 <sebastianlpasquini@gmail.com>
2020-04-09 12:47:06 -0300

    primer commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyDt4lhX3nQZfRzSHtDl8oq5CZ1lzSBfKhE',
+    authDomain: 'app4pps.firebaseapp.com',
+    databaseURL: 'https://app4pps.firebaseio.com',
+    projectId: 'app4pps',
+    storageBucket: 'app4pps.appspot.com',
+    messagingSenderId: '646893639055',
+    appId: '1:646893639055:web:9002f0c008e394657667fa'
+  }
 };
 
 /*
